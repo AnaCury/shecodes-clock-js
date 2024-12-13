021f40870cb210590190519bcf2b8b85b423d8b1
Ana Cury <75744584+AnaCury@users.noreply.github.com>
2024-12-13 10:15:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Sao Paulo
 const saoPaulo = document.querySelector("#sao");
 const saoPauloDate = saoPaulo.querySelector(".date");
 const saoPauloTime = saoPaulo.querySelector(".time");
@@ -5,12 +6,31 @@ const saoPauloHora = moment();
 saoPauloDate.innerHTML = moment().format("Do MMMM YYYY");
 saoPauloTime.innerHTML = `${saoPauloHora.format("h:mm:ss")}`;
 
-let parisElement = document.querySelector("#paris");
-if (parisElement) {
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
+//Paris
+const paris = document.querySelector("#paris");
+const parisDate = paris.querySelector(".date");
+const parisTime = paris.querySelector(".time");
+const parisHora = moment().tz("Europe/Paris");
+parisDate.innerHTML = moment().format("Do MMMM YYYY");
+parisTime.innerHTML = `${parisHora.format("h:mm:ss")}`;
 
-  parisDateElement.innerHTML = parisTime.format("Do MMMM YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss");
-}
+//Select City
+const citySelect = document.getElementById("city");
+const timeDisplay = document.getElementById("timeDisplay");
+
+citySelect.addEventListener("change", function () {
+  let timezone = citySelect.value;
+  let time;
+
+  if (timezone === "current") {
+    time = moment(); // hora local do usuário
+  } else if (timezone) {
+    time = moment.tz(timezone); // hora na cidade selecionada
+  } else {
+    timeDisplay.innerHTML = ""; // limpa o display se nada estiver selecionado
+    return;
+  }
+
+  // Exibe a hora formatada
+  timeDisplay.innerHTML = `Hora: ${time.format("HH:mm:ss")} em ${timezone}`;
+});
