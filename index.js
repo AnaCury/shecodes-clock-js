fe4ab40898d6a7442719efb5d720946bed8758c9
Ana Cury <75744584+AnaCury@users.noreply.github.com>
2024-12-12 20:58:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const saoPaulo = document.querySelector("#sao");
 const saoPauloDate = saoPaulo.querySelector(".date");
 const saoPauloTime = saoPaulo.querySelector(".time");
 const saoPauloHora = moment();
-saoPauloDate.innerHTML = moment().format("MMMM Do YYYY");
+saoPauloDate.innerHTML = moment().format("Do MMMM YYYY");
 saoPauloTime.innerHTML = `${saoPauloHora.format("h:mm:ss")}`;
-const beloHorizonte = document.querySelector("#belo");
+
+let parisElement = document.querySelector("#paris");
+if (parisElement) {
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss");
+}
